e74d59d6d9bc6f8d4eb087f5d6616d3be99eb9de
unknown <andressaszx@gmail.com>
2021-01-14 10:44:30 -0300

    Conclusion exercise 01 class69

---
+++ src/main/java/entities/Rectangle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Andressa Silva
+ */
+public class Rectangle {
+    
+    public double width;
+    public double height;
+    
+    public double area(){
+        return width * height;
+    }
+    
+    public double perimeter(){
+        return 2 * (width + height);
+    }
+    
+    public double diagonal(){
+        return Math.sqrt(width * width + height * height); 
+    }
+}
